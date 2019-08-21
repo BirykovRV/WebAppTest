74a029ee9fa2931cd104b646d5f8174c5f82f6ce
BirykovRV <r.v.birukov@yandex.ru>
2019-08-21 14:18:19 +0300

    signalR

--- WebAppTest/App_Start/BundleConfig.cs
+++ WebAppTest/App_Start/BundleConfig.cs
@@ -11,6 +11,9 @@ namespace WebAppTest
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/signalR").Include(
+                        "~/Scripts/jquery.signalR-{version}.js"));
+
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
